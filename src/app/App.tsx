47e2096bc412b0175c3489e88d43f8d91999cb4c
Felix Kirchhoff <mailfk@phispace.eu>
2021-11-04 10:35:41 +0100

    Add todo and theme styling

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,15 +1,43 @@
-import React from 'react'
-
-import Button from './components/Button/Button'
+import React, { useState } from 'react'
 import Title from './components/Title/Title'
+import Todo from './components/Todo/Todo'
 
 import classes from './App.module.css'
 
+type todoElement = {
+  isDone: boolean
+  title: string
+  description: string
+}
+
 function App(): JSX.Element {
+  const [todos, setTodos] = useState([
+    {
+      isDone: true,
+      title: 'Learn React',
+      description: 'lorem lorem lorem lorem lorem lorem lorem lorem ',
+    },
+    {
+      isDone: false,
+      title: 'Learn more React',
+      description: 'lorem lorem lorem lorem lorem lorem lorem lorem ',
+    },
+  ])
+
+  function handleClick(id: number) {
+    const newTodos = [...todos]
+    newTodos[id].isDone = !todos[id].isDone
+    setTodos(newTodos)
+  }
+
+  function renderTodo(todo: todoElement, id: number) {
+    return <Todo options={todo} onClick={() => handleClick(id)} />
+  }
+
   return (
     <div className={classes.app}>
       <Title>Remember Me</Title>
-      <Button>Hello</Button>
+      <ul className={classes.todoContainer}>{todos.map(renderTodo)}</ul>
     </div>
   )
 }
